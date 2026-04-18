63778a4b7d4d68458b9c5c242524c1c62fce53db
slender <2037779479@qq.com>
2026-04-18 13:58:53 +0800

    tree update

---
+++ src/main/kotlin/helper/Entity.kt
@@ -0,0 +1,5 @@
+package helper
+
+class Data {
+    fun update(value: Int){}
+}
--- src/main/kotlin/tree/Travel.kt
+++ src/main/kotlin/tree/Travel.kt
@@ -1,12 +1,13 @@
 package tree
 
+import helper.Data
 import helper.TreeNode
 
 /**
  * 二叉树的层序遍历（广度优先搜索）
  *
  * 时间复杂度 `O(n)`
- * 空间复杂度 `O(width)` 最坏情况为完全二叉树,需要存储n/2个节点
+ * 空间复杂度 `O(width)` 最坏情况为完全二叉树,需要存储`n/2`个节点
  *
  * 原理: 队列（双端队列）实现 BFS
  *
@@ -29,4 +30,29 @@ fun TreeNode.travelByBFS(action: TreeNode.(Int) -> Unit){
         }
         level++
     }
+}
+
+/**
+ * 二叉树深度优先遍历
+ *
+ * 前序遍历/传递型 处理问题时只需要父节点信息
+ *
+ * 通常需要成员变量来记录结果,数据通过系统栈隐式保存并传递
+ *
+ * 需要指定`dummy`节点信息(`root`节点的父节点)作为初始数据
+ *
+ * 遍历顺序：根节点 -> 左子树 -> 右子树
+ *
+ * 时间复杂度：`O(n)`
+ * 空间复杂度：`O(height)` 最坏情况为链表,方法递归`n`次
+ *
+ * @param data 父节点信息
+ * @param action 接收父节点信息,处理每个节点
+ */
+fun TreeNode.travelByDFS(data: Data, action: TreeNode.(Any?) -> Unit){
+    action(data)
+    //更新数据并向下传递
+    data.update(value)
+    left?.travelByDFS(data,action)
+    right?.travelByDFS(data,action)
 }
